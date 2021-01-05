3aee3804f9032c2350237cd732cd45af557a207c
ramaprasath <ramaprasath@interactionone.com>
2021-01-05 22:31:07 +0530

    Practice - Ruby

---
+++ Ruby/ruby.rb
@@ -0,0 +1,13 @@
+class Animal
+    def initialize
+        puts "This is Animal Class"
+    end
+    def setName(newName)
+
+    end
+    def getName
+        @name
+    end
+    def name
+        @name
+    end
